7a97c562c71f1485f11366737bfb2e0b3a5a5f7e
mac-ohle <hostmaster@mizan.com>
2020-07-07 16:47:16 +0700

    update modul percobaan/testrun

--- application/modules/percobaan/controllers/Testrun.php
+++ application/modules/percobaan/controllers/Testrun.php
@@ -8,6 +8,20 @@ class Testrun extends MX_Controller {
 		$var1 = modules::run('percobaan/welkam/test_run', $param = 'ucing...');
 		echo $var1;
 	}
+
+
+	public function ambilNilai($var1 = NULL, $var2 = NULL)
+	{
+		$pesan1 = 'Ini respon dari fungsi percobaan/controllers/testrun/ambilNilai() via modules::run ';
+		$pesan2 = NULL;
+		if (!is_null($var1)) {
+			$pesan2 = $var1;
+		}
+		if (!is_null($var2)) {
+			$pesan2 .= ' dan ' . $var2;
+		}
+		return [$pesan1,$pesan2];
+	}
 }
 
 /* Testrun.php */
